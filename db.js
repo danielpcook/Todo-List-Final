f19f1acb5e3d3a3d0a5a7932804ea52164d97691
Daniel Cook <dpcook125@gmail.com>
2024-05-06 13:25:00 -0500

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise')
 const db = mysql. createPool({
     host: "127.0.0.1",
     user: "root",
-    password: "Playball0109@",
+    password: "",
     database: "taskdata"
 })
 
